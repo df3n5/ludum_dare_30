75499b747a4e68770dfb1d175a0f915179b4e686
Jonathan Frawley <jonathan@digitgaming.com>
2014-08-23 14:23:37 +0100

    Good stuff

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -10,11 +10,17 @@ import flixel.group.FlxGroup;
 import flixel.tile.FlxTilemap;
 
 class PlayState extends FlxState {
+    var tiledLevel:TiledLevel;
+
     public function new(levelNo:Int, glitched:Bool, died:Bool):Void {
         super();
     }
 
     public override function create():Void {
+        tiledLevel = new TiledLevel("assets/tiled/portal.tmx");
+        tiledLevel.loadObjects(this);
+        // Add tilemaps
+        add(tiledLevel.foregroundTiles);
     }
 
     override public function update():Void {
